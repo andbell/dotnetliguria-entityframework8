c7df9ec2038feb1616588896a09a593b70bec27a
Andrea Belloni <andreabelloni77@outlook.com>
2024-01-18 21:55:04 +0100

    feat: DateOnly and TimeOnly

--- DotNetLiguria.EF8/Migrations/20240118202948_UpdateOrder.Designer.cs
+++ DotNetLiguria.EF8/Migrations/20240118202948_UpdateOrder.Designer.cs
@@ -5,6 +5,7 @@ using DotNetLiguria.EF8;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.SqlServer.Types;
 
@@ -13,9 +14,11 @@ using Microsoft.SqlServer.Types;
 namespace DotNetLiguria.EF8.Migrations
 {
     [DbContext(typeof(EF8Context))]
-    partial class EF8ContextModelSnapshot : ModelSnapshot
+    [Migration("20240118202948_UpdateOrder")]
+    partial class UpdateOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DotNetLiguria.EF8/Models/Order.cs
+++ DotNetLiguria.EF8/Models/Order.cs
@@ -6,12 +6,18 @@ public class Order
 {
     public int Id { get; set; }
     public required int CustomerId { get; set; }
+
     [Required]
     public required string Contents { get; set; }
-    [Required]
+
     public required Address ShippingAddress { get; set; }
-    [Required] 
+    
     public required Address BillingAddress { get; set; }
 
     public virtual Customer Customer { get; set; }
+
+    [Required]
+    public DateOnly Date { get; set; }
+
+    public TimeOnly? PreferredDeliveryTime { get; set; }
 }
